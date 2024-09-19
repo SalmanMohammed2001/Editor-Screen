7fff7ab07d7506d3a7d3a10c2ee650141d455a46
salman <salmannisthar123@gmail.com>
2024-09-19 23:07:47 +0530

    setup layout

---
+++ src/layout/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div className='h-[100vh] flex '>
+        <div className='h-[100%] w-[365px] border border-black'> 
+          
+           </div>
+        <div className='h-[100%] flex-1  border'> 
+      
+        </div>
+    </div>
+  )
+}
+
+export default Layout
